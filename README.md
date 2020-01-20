0ad59349c3b9279c31ca564ae987cdb7b5c04b9b
jxzhsiwuxie <331924657@qq.com>
2020-01-20 20:40:28 +0800

    更新1.2节示例以及练习

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # cppPrimer
-C++Primer5 exercises
+C++Primer5 练习题
 
 ## 该仓库主要包含两部分
 1. 书中的示例程序，对应的源文件放在每一章节文件夹下的 `examples` 子文件夹中
---
+++ chap01/exercises/ex_1.3..cpp
@@ -0,0 +1,10 @@
+//练习 1.3：编写程序，在标准输出上打印 Hello, World。
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World." << std::endl;
+
+    return 0;
+}

59760a99f8b7ac2d3696c7610db7f3834410dc1e
jxzhsiwuxie <331924657@qq.com>
2020-02-05 23:31:31 +0800

    新增，4.8 节练习。

---
+++ chap04/exercises/ex_4.27.cpp
@@ -0,0 +1,11 @@
+//练习 4.27：下列表达式的结果是什么？
+// unsigned long ul1 = 3, ul2 = 7;
+// (a) ul1 & ul2        (b) ul1 | ul2
+// (c) ul1 && ul2       (d) ul2 || ul2
+
+
+/*
+* 0011    0111
+* (a) 3         (b) 7
+* (c) true      (d) true
+*/

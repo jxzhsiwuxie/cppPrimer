68843cf64c0a2ddb63e2e5a50bd1e2bcb19ae9de
jxzhsiwuxie <331924657@qq.com>
2020-03-11 22:33:22 +0800

    新增，9.3.6 节示例代码。

---
+++ chap09/samples/sp_9.3.6.cpp
@@ -0,0 +1,22 @@
+//删除 vector 中的偶数，复制其中的奇数。
+
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+int main() {
+    vector<int> vi = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    auto iter = vi.begin();
+    while (iter != vi.end()) {
+        if (*iter % 2) {
+            vi.insert(iter, *iter);
+            iter += 2;
+        } else
+            iter = vi.erase(iter);
+    }
+
+    return 0;
+}

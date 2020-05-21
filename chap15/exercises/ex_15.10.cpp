c61367d0e41c20f054936254cc5e8fa3c9d5ea49
jxzhsiwuxie <331924657@qq.com>
2020-05-21 23:00:59 +0800

    新增，15.2.3 节练习。

---
+++ chap15/exercises/ex_15.10.cpp
@@ -0,0 +1,5 @@
+//练习 15.10：会议我们在 8.1 节（第 279 页）进行的讨论，解释将 284 页中 ifstream 传递给 Sales_data 的 read 函数的程序是如何工作的。
+
+/*
+* read 形参是 istream&，ifstream 是 istream 的派生类，自然可以将一个 istream 类型的引用绑定到 ifstream 类型的对象上。
+*/

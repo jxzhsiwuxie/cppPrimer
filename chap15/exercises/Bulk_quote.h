fd6e55c4c74f05c5a80b90fd251868963666d347
jxzhsiwuxie <331924657@qq.com>
2020-05-24 12:23:53 +0800

    新增，15.7.4 节练习。

--- chap15/exercises/Bulk_quote.h
+++ chap15/exercises/Bulk_quote.h
@@ -6,7 +6,8 @@
 class Bulk_quote : public Disc_quote {
    public:
     Bulk_quote() = default;
-    Bulk_quote(const std::string&, double, std::size_t, double);
+    // Bulk_quote(const std::string& book, double p, std::size_t n, double disc) : Disc_quote(book, p, n, disc) {}
+    using Disc_quote::Disc_quote;
     ~Bulk_quote() = default;
 
     //覆盖基类的函数版本以实现基于大量购买的折扣政策
@@ -36,7 +37,6 @@ class Bulk_quote : public Disc_quote {
     }
 };
 
-Bulk_quote::Bulk_quote(const std::string& book, double p, std::size_t n, double disc) : Disc_quote(book, p, n, disc) {}
 
 double Bulk_quote::net_price(std::size_t cnt) const {
     if (cnt >= quatity)
---
+++ chap15/exercises/ex_15.27.cpp
@@ -0,0 +1,3 @@
+//练习 15.27：重新定义你的 Bulk_quote 类，令其继承构造函数。
+
+#include "Bulk_quote.h"

7a4280eefe2403458129c1e3139e9dc44a6df016
jxzhsiwuxie <331924657@qq.com>
2020-06-01 22:06:17 +0800

    新增，16.1.5 节练习。

---
+++ chap16/exercises/ex_16.25.cpp
@@ -0,0 +1,9 @@
+//练习 16.25：解释下面这些声明的含义。
+// extern template class vector<string>;
+// template class vector<Sales_data>;
+
+
+/*
+* extern template class vector<string>; 表示模板实例化的声明。
+* template class vector<Sales_data>; 表示模板实例化的定义。
+*/
---
+++ chap16/exercises/ex_16.27.cpp
@@ -0,0 +1,17 @@
+//练习 16.27：对下面每条带标签的语句，解释发生了什么样的实例化（如果有的话）。如果一个模板被实例化，
+//解释为什么；如果未实例化，解释为什么没有。
+// template <typename T> class Stack{};
+// void f1(Stack<char>);                     //(a)
+// class Exercise {
+//    Stack<double> &rsd;                   //(b)
+//    Stack<int> si;                        //(c)
+// };
+// int main (){
+//     Stack<char> *sc;                     //(d)
+//     f1(*sc);                             //(e)
+//     int iObj = sizeof(Stack<string>);   //(f)
+// }
+
+/*
+* 这题不太懂。
+*/

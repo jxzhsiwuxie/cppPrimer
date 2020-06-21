e8f47a98b86e70745ccbe4f29bcf71627cb12494
jxzhsiwuxie <331924657@qq.com>
2020-06-21 22:58:46 +0800

    新增，17.4.2 节练习。

---
+++ chap17/exercises/ex_17.33.cpp
@@ -0,0 +1,91 @@
+//练习 17.33：修改 11.3.6 节（第 392 页）中的单词转换程序，允许对一个给定单词有多种转换方式，每次随机选择一种进行实际转换。
+
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <random>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+using std::bernoulli_distribution;
+using std::cin;
+using std::cout;
+using std::default_random_engine;
+using std::endl;
+using std::getline;
+using std::ifstream;
+using std::istringstream;
+using std::map;
+using std::runtime_error;
+using std::string;
+
+//生成转换规则
+map<string, string> buildMap(ifstream &map_file) {
+    map<string, string> trans_map;  //保存转换规则
+    string key;                     //要转换的单词
+    string value;                   //转换你后的内容
+    //读取第一个单词存入 key 中，行中剩余内容存入 value 中
+    while (map_file >> key && getline(map_file, value)) {
+        if (value.size() > 1)                  //检查是否有转换规则
+            trans_map[key] = value.substr(1);  //跳过前导空格
+        //trans_map.insert({key,value.substr(1)});
+        else
+            throw runtime_error("no rule for " + key);
+    }
+    return trans_map;
+}
+
+//生成转换文本
+const string &transform(const string &s, const map<string, string> &m) {
+    //实际的转化工作
+    auto map_it = m.find(s);
+    if (map_it != m.end())
+        return map_it->second;  //使用替换短语
+    else
+        return s;  //否则返回原 string
+}
+
+void word_transform(ifstream &map_file, ifstream &input) {
+    auto trans_map = buildMap(map_file);  //保存转换规则
+    string text;                          //保存输入中的每一行
+    while (getline(input, text)) {        //读取一行输入
+        istringstream stream(text);       //读取每个单词
+        string word;
+        bool firstword = true;  //控制是否打印空格
+        while (stream >> word) {
+            if (firstword)
+                firstword = false;
+            else
+                cout << " ";
+
+            cout << transform(word, trans_map);  //打印输出
+        }
+        cout << endl;  //完成一行的转换
+    }
+}
+
+int main() {
+    ifstream map_ifs;
+    ifstream input_ifs0("input.txt");
+    ifstream input_ifs1("input.txt");
+    string line;
+
+    default_random_engine e(std::time(nullptr));
+    bernoulli_distribution b;
+    if (b(e))
+        map_ifs = ifstream("map_file.txt");
+    else
+        map_ifs = ifstream("map_file2.txt");
+
+    cout << "转换前的文本为：" << endl;
+    while (getline(input_ifs0, line))
+        cout << line << endl;
+    cout << "------------------------" << endl;
+
+    cout << "转换后的文本为：" << endl;
+    word_transform(map_ifs, input_ifs1);
+
+    return 0;
+}
